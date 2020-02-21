adf8bb7f1859d8b6fc55d3ee0a5a9712882b7d56
msenkpiel <marco.senkpiel@gmail.com>
2020-02-21 07:39:35 +0100

    initial commit

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,18 @@
+<div id="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                One of three columns
+            </div>
+            <div class="col-sm">
+                One of three columns
+            </div>
+            <div class="col-sm">
+                One of three columns
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
